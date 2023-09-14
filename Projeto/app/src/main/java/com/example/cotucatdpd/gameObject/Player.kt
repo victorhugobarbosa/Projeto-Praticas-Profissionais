9bc905d96f41699fec4a0451dd408703d474a8d5
Pedro Nunes <zyxblackbeard@gmail.com>
2023-09-14 07:19:45 -0300

    started making the animation

--- Projeto/app/src/main/java/com/example/cotucatdpd/gameObject/Player.kt
+++ Projeto/app/src/main/java/com/example/cotucatdpd/gameObject/Player.kt
@@ -9,7 +9,7 @@ import com.example.cotucatdpd.R
 import com.example.cotucatdpd.gamePanel.*
 import com.example.cotucatdpd.graphics.Sprite
 
-class Player(context: Context?, positionX: Double, positionY: Double, radius: Double, joystick: Joystick, sprite: Sprite)
+    class Player(context: Context?, positionX: Double, positionY: Double, radius: Double, joystick: Joystick, animator: Animator)
     : Circle(ContextCompat.getColor(context!!, R.color.player), positionX, positionY, radius) {
 
     val SPEED_PIXELS_PER_SECOND = 400.0
@@ -18,7 +18,7 @@ class Player(context: Context?, positionX: Double, positionY: Double, radius: Do
     private var joystick = joystick
     private var healthBar = HealthBar(this, context)
     private var healthPoints = MAX_HEALTH_POINTS
-    private var sprite = sprite
+    private var animator = animator
 
     override fun update(){
         velocityX = joystick.getActuatorX()!! *MAX_SPEED
@@ -45,9 +45,7 @@ class Player(context: Context?, positionX: Double, positionY: Double, radius: Do
     }
 
     override fun draw(canvas: Canvas?, gameDisplay: GameDisplay){
-        sprite!!.draw(canvas,
-                    gameDisplay.gameToDisplayX(getPositionX()) - sprite.getWidth()/2,
-                    gameDisplay.gameToDisplayY(getPositionY()) - sprite.getHeight()/2)
+        animator.draw(canvas, gameDisplay, this)
         healthBar.draw(canvas, gameDisplay)
     }
 }
