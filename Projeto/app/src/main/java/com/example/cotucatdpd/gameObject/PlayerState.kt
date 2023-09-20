7acc5e34a9dda6cf82beb971d69d4c9d4f6aa9f5
Pedro Nunes <zyxblackbeard@gmail.com>
2023-09-20 07:23:21 -0300

    some little changes

--- Projeto/app/src/main/java/com/example/cotucatdpd/gameObject/PlayerState.kt
+++ Projeto/app/src/main/java/com/example/cotucatdpd/gameObject/PlayerState.kt
@@ -2,7 +2,7 @@ package com.example.cotucatdpd.gameObject
 
 class PlayerState(player: Player) {
 
-    enum class State{
+    public enum class State{
         NOT_MOVING,
         STARTED_MOVING,
         IS_MOVING
