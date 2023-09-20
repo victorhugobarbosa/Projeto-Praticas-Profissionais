d7cb10d7e4deed0830e8df3a669595af95a21cd9
Pedro Nunes <zyxblackbeard@gmail.com>
2023-09-20 09:06:24 -0300

    Revert "Revert "some little changes""
    This reverts commit e9bd55d252cce4404e4d7cfc9c4010682ed46f9b.

--- Projeto/app/src/main/java/com/example/cotucatdpd/gameObject/Player.kt
+++ Projeto/app/src/main/java/com/example/cotucatdpd/gameObject/Player.kt
@@ -7,6 +7,7 @@ import com.example.cotucatdpd.GameDisplay
 import com.example.cotucatdpd.GameLoop
 import com.example.cotucatdpd.R
 import com.example.cotucatdpd.gamePanel.*
+import com.example.cotucatdpd.graphics.Animator
 import com.example.cotucatdpd.graphics.Sprite
 
     class Player(context: Context?, positionX: Double, positionY: Double, radius: Double, joystick: Joystick, animator: Animator)
@@ -19,6 +20,7 @@ import com.example.cotucatdpd.graphics.Sprite
     private var healthBar = HealthBar(this, context)
     private var healthPoints = MAX_HEALTH_POINTS
     private var animator = animator
+    private var state = PlayerState(this)
 
     override fun update(){
         velocityX = joystick.getActuatorX()!! *MAX_SPEED
@@ -34,6 +36,8 @@ import com.example.cotucatdpd.graphics.Sprite
             directionX = velocityX!! / distance
             directionY = velocityY!! / distance
         }
+
+        state.update()
     }
 
     fun getHealthPoints() : Int{
@@ -42,6 +46,9 @@ import com.example.cotucatdpd.graphics.Sprite
     fun setHealthPoints(newHp: Int){
         if(newHp >= 0 )
             healthPoints = newHp
+    }
+    fun getState() : PlayerState{
+        return state
     }
 
     override fun draw(canvas: Canvas?, gameDisplay: GameDisplay){
--- Projeto/app/src/main/java/com/example/cotucatdpd/graphics/Animator.kt
+++ Projeto/app/src/main/java/com/example/cotucatdpd/graphics/Animator.kt
@@ -3,15 +3,42 @@ package com.example.cotucatdpd.graphics
 import android.graphics.Canvas
 import com.example.cotucatdpd.GameDisplay
 import com.example.cotucatdpd.gameObject.Player
+import com.example.cotucatdpd.gameObject.PlayerState
 
 class Animator(playerSpriteArray: ArrayList<Sprite>) {
     private var playerSpriteArray = playerSpriteArray
+    private var idNotMovingFrame = 0
+    private var idMovingFrame = 1
+    private var updatesBeforeNextFrame = 0
+    private val MAX_UPDATES_BEFORE_NEXT_FRAME = 5
 
     fun draw(canvas: Canvas?, gameDisplay: GameDisplay, player: Player){
-        when(state){
-
+        when(player.getState().getState()){
+            PlayerState.State.NOT_MOVING ->{
+                drawFrame(canvas, gameDisplay, player, playerSpriteArray[idNotMovingFrame])
+            }
+            PlayerState.State.STARTED_MOVING ->{
+                updatesBeforeNextFrame = 5
+                drawFrame(canvas, gameDisplay, player, playerSpriteArray[idMovingFrame])
+            }
+            PlayerState.State.IS_MOVING ->{
+                updatesBeforeNextFrame--
+                if(updatesBeforeNextFrame == 0){
+                    updatesBeforeNextFrame = MAX_UPDATES_BEFORE_NEXT_FRAME
+                    toggleIdMovingFrame()
+                }
+                drawFrame(canvas, gameDisplay, player, playerSpriteArray[idMovingFrame])
+            }
+            else -> {return}
         }
     }
+    fun toggleIdMovingFrame(){
+        if(idMovingFrame == 1)
+            idMovingFrame = 2
+        else
+            idMovingFrame = 1
+    }
+
     fun drawFrame(canvas: Canvas?, gameDisplay: GameDisplay, player: Player, sprite: Sprite){
         sprite.draw(canvas,
             gameDisplay.gameToDisplayX(player.getPositionX()) - sprite.getWidth()/2,
